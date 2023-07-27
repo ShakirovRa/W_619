3fb5f79e312de6bbbf9bb33dda2bef410c647c79
ShakirovRa <68635549+ShakirovRa@users.noreply.github.com>
2023-07-27 22:54:05 +0300

    Update Test619-HTML5-Shipping.UE4.js

--- Test619-HTML5-Shipping.UE4.js
+++ Test619-HTML5-Shipping.UE4.js
@@ -1133,7 +1133,7 @@ $(document).ready(function() {
 				return { db: db, wasmBytes: wasmBytes, fromIndexedDB: true };
 			});
 		}).catch(function() {
-			return download(Module.locateFile('UE4Game-HTML5-Shipping.wasm'), 'arraybuffer').then(function(wasmBytes) {
+			return download(Module.locateFile('https://dl.dropboxusercontent.com/scl/fi/2sulndcwbfo9ldjjmgt3m/UE4Game-HTML5-Shipping.wasm?rlkey=njmk4hqohqrnfmpvpfwesxj8x&dl=0'), 'arraybuffer').then(function(wasmBytes) {
 				return { db: db, wasmBytes: wasmBytes, fromIndexedDB: false };
 			});
 		});
@@ -1167,9 +1167,9 @@ $(document).ready(function() {
 			});
 */
 // Instead as a fallback, download as ArrayBuffer. (TODO: Figure out the bugs with the above, and switch to using that one instead)
-			fetchOrDownloadAndStore(db, Module.locateFile('Test619-HTML5-Shipping.data'), 'arraybuffer').then(function(dataArrayBuffer) {
+			fetchOrDownloadAndStore(db, Module.locateFile('https://dl.dropboxusercontent.com/scl/fi/8xlxs732ukup7fl9spsjn/Test619-HTML5-Shipping.data?rlkey=hgw8fo3iu809accplq44qq7g4&dl=0'), 'arraybuffer').then(function(dataArrayBuffer) {
 				Module['preloadedPackages'] = {};
-				Module['preloadedPackages'][Module.locateFile('Test619-HTML5-Shipping.data')] = dataArrayBuffer;
+				Module['preloadedPackages'][Module.locateFile('https://dl.dropboxusercontent.com/scl/fi/8xlxs732ukup7fl9spsjn/Test619-HTML5-Shipping.data?rlkey=hgw8fo3iu809accplq44qq7g4&dl=0')] = dataArrayBuffer;
 				return dataJsDownload.then(addScriptToDom);
 			});
 
